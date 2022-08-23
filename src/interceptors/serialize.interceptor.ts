8b83b313f2fb8f395b01adc743e417e222f78268
mez32 <amesmer32@gmail.com>
2022-08-23 18:06:45 -0400

    Added user routing and serialization of responses

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance, ClassConstructor } from 'class-transformer';
+
+export function Serialize<T>(dto: ClassConstructor<T>) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<T> {
+    return handler.handle().pipe(
+      map((data: T) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
